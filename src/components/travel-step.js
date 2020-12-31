3c996ef133e4813a798c7a91ee274bb558ccb886
Katie Quesnel <katie_quesnel@yahoo.ca>
2020-12-31 12:16:56 -0400

    the small progress made to allow clearing of data

--- src/components/travel-step.js
+++ src/components/travel-step.js
@@ -1,4 +1,5 @@
 import React from "react";
+import directives from '../data/dummy-directives'
 
 const TravelStep = ({data, index}) => {
     return (
---
+++ src/data/dummy-directives.js
@@ -0,0 +1,14 @@
+module.exports = {
+    D1: {
+        label: "Directive One",
+        link: "/d-one"
+    },
+    D2: {
+        label: "Directive Two",
+        link: "/d-two"
+    },
+    D3: {
+        label: "Directive Three",
+        link: "/d-three"
+    },
+};
